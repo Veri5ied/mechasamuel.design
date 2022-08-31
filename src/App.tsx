8a7235b17e63e4140161d742ba42c1186b29e927
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-08-31 21:18:55 +0100

    landing page update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import TopNavigation from "./components/ui/top-navigation/TopNavigation";
 import { LandingPage } from "./pages";
 import { LANDING_PAGE } from "./routes";
 import { Routes, Route } from "react-router-dom";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
-    <div className="container">
-      <TopNavigation />
-      <Routes>
-        <Route path={LANDING_PAGE} element={<LandingPage />} />
-      </Routes>
-    </div>
+    <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+      <div className="container">
+        <TopNavigation />
+        <Routes>
+          <Route path={LANDING_PAGE} element={<LandingPage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
